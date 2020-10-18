06c759fbb2ce37e7f91bc9b0f87b5cd0ff7b84cf
goodfloki <lukaskirstukas@gmail.com>
2020-10-18 20:01:53 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./style.css";
 import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 
 export default function App() {
   return (
     <div class="wrapper">
+    <TextField id="standard-basic" label="Email">  </TextField>
+    <TextField id="standard-basic" label="Password">  </TextField>
       <Button variant="contained"> Login </Button>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
     </div>
   );
 }
